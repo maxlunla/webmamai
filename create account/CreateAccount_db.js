55b471de1c1569a86533ffd8ebc06ad3679374f0
warabai <32844501+60070086@users.noreply.github.com>
2020-04-15 02:13:20 +0700

    1

---
+++ create account/CreateAccount_db.js
@@ -0,0 +1,9 @@
+//const db=firebase.firestore();
+//const table=document.querySelector('#')
+
+const form=document.querySelector('#Studentfrom');
+
+
+form.addEventListener('submit',(e)=>{
+	console.log(form.name.value);
+});
